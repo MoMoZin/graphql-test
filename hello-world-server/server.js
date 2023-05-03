f6a0183de592828c143786c293827deaa6c6d1e7
MoMoZin <khinmomozin@gmail.com>
2023-05-03 15:35:38 -0600

    configure graphql and apollo

---
+++ hello-world-server/server.js
@@ -0,0 +1,10 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const port = process.env.PORT || 8000;
+const app = express();
+
+//register middleware
+app.use(bodyParser.json(), cors());
+app.listen(port, () => console.log(`server is up and running at ${port}`));
+
