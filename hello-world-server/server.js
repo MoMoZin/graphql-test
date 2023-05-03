94251eef972de6d3a492c08ba8a3ebe5412e6154
MoMoZin <khinmomozin@gmail.com>
2023-05-03 15:49:47 -0600

    greeting api call setup

--- hello-world-server/server.js
+++ hello-world-server/server.js
@@ -8,3 +8,25 @@ const app = express();
 app.use(bodyParser.json(), cors());
 app.listen(port, () => console.log(`server is up and running at ${port}`));
 
+// Adding Type Definitions
+const typeDefinition = `
+   type Query  {
+      greeting: String
+   }`;
+
+// Adding resolver
+const resolverObject = {
+  Query: {
+    greeting: () => 'Hello GraphQL  From TutorialsPoint !!'
+  }
+};
+
+// bind the schema and resolver
+const { makeExecutableSchema } = require('graphql-tools');
+const schema = makeExecutableSchema({ typeDefs: typeDefinition, resolvers: resolverObject });
+
+
+// Define Routes to Fetch Data from ReactJS/GraphiQL Application
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+app.use('/graphql', graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
