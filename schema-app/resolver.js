b6611e803342ba4d87f56f895a027e954aebecad
MoMoZin <khinmomozin@gmail.com>
2023-05-07 21:16:50 -0600

    add first mutation to add student

--- schema-app/resolver.js
+++ schema-app/resolver.js
@@ -6,36 +6,46 @@ const Query = {
 
   },
 
-  //resolver function for students returns list
-  students: () => db.students.list(),
+  // //resolver function for students returns list
+  // students: () => db.students.list(),
 
-  //resolver function for studentbyId
-  studentById: (rrot, args, context, info) => {
-    //args will contain parameter passed in query
-    return db.students.get(args.id);
-  },
+  // //resolver function for studentbyId
+  // studentById: (rrot, args, context, info) => {
+  //   //args will contain parameter passed in query
+  //   return db.students.get(args.id);
+  // },
 
-  sayHello: (root, args, context, info) => {
-    return `Hi ${args.name} GraphQL server says Hello to you!!`;
-  },
+  // sayHello: (root, args, context, info) => {
+  //   return `Hi ${args.name} GraphQL server says Hello to you!!`;
+  // },
 
-  setFavouriteColor: (root, args) => {
-    return "Your Fav Color is :" + args.color;
-  }
+  // setFavouriteColor: (root, args) => {
+  //   return "Your Fav Color is :" + args.color;
+  // }
 
 };
 
-
-
-
-//for each single student object returned,resolver is invoked
-const Student = {
-  fullName: (root, args, context, info) => {
-    return root.firstName + ":" + root.lastName;
-  },
-  college: (root) => {
-    return db.colleges.get(root.collegeId);
+const Mutation = {
+  createStudent: (root, args, context, info) => {
+    return db.students.create({
+      collegeId: args.collegeId,
+      firstName: args.firstName,
+      lastName: args.lastName
+    });
   }
 };
 
-module.exports = { Query, Student };
+
+
+// //for each single student object returned,resolver is invoked
+// const Student = {
+//   fullName: (root, args, context, info) => {
+//     return root.firstName + ":" + root.lastName;
+//   },
+//   college: (root) => {
+//     return db.colleges.get(root.collegeId);
+//   }
+// };
+
+// module.exports = { Query, Student };
+module.exports = { Query, Mutation };
